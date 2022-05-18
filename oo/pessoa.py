f97735cc9adc4cca3b8b763aa2c0a2a8ef0c803a
ednai <47866756+ednai@users.noreply.github.com>
2022-05-18 16:40:56 -0300

    Criado  atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,8 @@
 class Pessoa:
+    olhos = 2 #atributo classe defoult(padrao) comunm a todos da classe pessoa 2 olhos
     def __init__(self, *filhos,nome=None,idade=35):
         self.idade = idade
-        self.nome = nome
+        self.nome = nome #atributo de instancia
         self.filhos = list(filhos)
 
     def cumprimentar(self):
@@ -19,7 +20,15 @@ if __name__=='__main__':
         print(filho.nome)
     luciano.sobrenome = 'Ramalho'
     del luciano.filhos
-    print(luciano.__dict__)
+    luciano.olhos=1 # atributos criando dinamicamente atraves da atribuicao
+    del luciano.olhos # removido (apaga) o atributo olhos do objeto luciano dianmicamente
     print(renzo.__dict__)
+    print(luciano.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos),id(luciano.olhos),id(renzo.olhos))
+
 
 
